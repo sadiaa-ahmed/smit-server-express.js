e1954c040431aa41fe9ee4411e5dcb4b9b16aa9d
Sadia Ahmed <sadiaahmed26373@gmail.com>
2025-03-25 22:47:10 -0700

    hello world code

---
+++ Express Js/server.mjs
@@ -0,0 +1,14 @@
+console.log("This is express server");
+
+import express from 'express';
+const app = express()
+// const port =  3000
+app.get('/', (req, res) => {
+    console.log("Hello" , new Date())
+    res.send('Hello World!' + new Date())
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
